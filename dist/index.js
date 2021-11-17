80d7f2bb76fabc1f614c5b6d06ca2aa3fea49016
Marko Klemetti <marko.klemetti@gmail.com>
2021-11-17 18:50:23 +0200

    fix(ci): fix semicolon places

--- dist/index.js
+++ dist/index.js
@@ -8475,12 +8475,15 @@ const resolver = __nccwpck_require__(9144);
 
     const { headRef, baseRef } = await resolver.resolveRefs(githubToken);
 
+    console.log(`headRef this: ${headRef}`);
+    console.log(`headRef  env: ${process.env.GITHUB_HEAD_REF}`);
+
     core.setOutput('base_ref', baseRef);
     core.setOutput('head_ref', headRef);
   } catch (error) {
     core.setFailed(error.message);
   }
-});
+})();
 
 })();
 
