484e27584d38effa18bc8d462adffb7f9149be8a
Marko Klemetti <marko.klemetti@gmail.com>
2021-11-17 19:21:51 +0200

    feat(ci): add outcome to pr verify

--- dist/index.js
+++ dist/index.js
@@ -8475,9 +8475,6 @@ const resolver = __nccwpck_require__(9144);
 
     const { headRef, baseRef } = await resolver.resolveRefs(githubToken);
 
-    console.log(`headRef this: ${headRef}`);
-    console.log(`headRef  env: ${process.env.GITHUB_HEAD_REF}`);
-
     core.setOutput('base_ref', baseRef);
     core.setOutput('head_ref', headRef);
   } catch (error) {
