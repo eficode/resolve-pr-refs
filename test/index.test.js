0b0541c5d9e66b36c0f993684731d0a2638d26f5
Marko Klemetti <marko.klemetti@gmail.com>
2021-11-17 17:55:50 +0200

    feat(test): mock github libraries

--- test/index.test.js
+++ test/index.test.js
@@ -1,32 +1,21 @@
 const resolver = require('../src/resolver');
 
-const contextPayload = {
-  payload: { issue: { pull_request: { url: '' } } },
-};
-
-const octokitPayload = {
-  status: 'example status',
-  data: {
-    base: { ref: 'test base ref' },
-    head: { ref: 'test head ref' },
-  },
-};
-
-jest.mock('@actions/github', () => ({
-  get context () {
-    return contextPayload;
-  },
-  getOctokit () {
-    return { request: () => octokitPayload };
-  },
-}));
+const mockPayload = require('./payload-provider');
 
 describe('resolveRefs', () => {
-  const token = "FAKETOKEN";
+  const token = process.env.GITHUB_TOKEN || "TEST TOKEN";
 
   it('should return base_ref', async () => {
+    // mockPayload.setOctokitPayload({ data: { base: { ref: 'test  ref' } } });
+
     const { baseRef } = await resolver.resolveRefs(token);
 
-    expect(baseRef).toEqual('test base ref');
+    expect(baseRef).toEqual(mockPayload.octokitPayload.data.base.ref);
+  });
+
+  it('should return head_ref', async () => {
+    const { headRef } = await resolver.resolveRefs(token);
+
+    expect(headRef).toEqual('test head ref');
   });
 });
