18bd74f18e53cc157006fa2a88be16262435825d
Marko Klemetti <marko.klemetti@gmail.com>
2021-11-22 11:31:25 +0200

    update prurl return types

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,33 +1,22 @@
 const github = require('@actions/github');
 
 const ISSUE_TYPES = ['issue_comment'];
+const PULL_REQUEST_TYPES = ['pull_request'];
 
 const getPrUrl = (payload: any) => {
-  try {
-    if (ISSUE_TYPES.includes(payload.event_name)) {
-      return github.context.payload.issue.pull_request.url;
-    }
-
-    return github.context.payload.pull_request.url;
-  } catch {
-    return null;
+  if (ISSUE_TYPES.includes(payload.event_name)) {
+    return github.context.payload.issue.pull_request.url;
   }
-};
 
-const getFallbackRefs = () => {
-  return {
-    status: null,
-    headRef: process.env.GITHUB_HEAD_REF,
-    baseRef: process.env.GITHUB_BASE_REF,
-  };
+  if (PULL_REQUEST_TYPES.includes(payload.event_name)) {
+    return github.context.payload.pull_request.url;
+  }
+
+  throw new Error(`Unknown event type: ${payload.event_name}`);
 };
 
 const resolveRefs = async (token: string) => {
   const prUrl = getPrUrl(github.context.payload);
-
-  if (!prUrl) {
-    return getFallbackRefs();
-  }
 
   const octokit = github.getOctokit(token);
 
